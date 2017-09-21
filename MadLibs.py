d1c18377286393b7d96cb09dec301819277e4ea1
PundoJnr <pundoarthur@gmail.com>
2017-09-21 05:33:05 -0700

    initial commit

---
+++ MadLibs.py
@@ -0,0 +1,5 @@
+a = input("Enter your name: ")
+b = input ("Enter your Age: ")
+c = input("Enter your favorite language: ")
+d = input("Enter your dream company: ")
+print ("Hey", a, "as you are",b,"years of age and aspire to work as a",c ,"programmer in",d ,"company you have to put in work! All the best.")
